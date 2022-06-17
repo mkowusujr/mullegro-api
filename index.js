d83aa81a866bbde34545935981aab59c97d6db9a
mathew owusu jr <mrfriiday@gmail.com>
2022-06-17 02:54:46 -0400

    renamed main js file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const db = require('./util/database')
 
+
 // start db
 const User = require('./models/user')
+const Post = require('./models/post')
+const Cart = require('./models/cart')
+const Transcation = require('./models/transcation')
 db
-    .sync()
+    .sync({ force: true })
     .then(() => {
         console.log('Database synchronized');
     })
@@ -12,12 +16,14 @@ db
         console.log(err)
     });
 
+
 // start app
 const app = express();
 const PORT = 3000;
 app.listen(
     PORT, () => console.log(`Server is live at http://localhost:${PORT}`)
 );
+
 
 // setup routes
 const homeRoute = require('./routes/home');
