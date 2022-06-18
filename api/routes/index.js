e210d3562d00395d7e62b0345f6c937a47a97d5f
mathew owusu jr <mrfriiday@gmail.com>
2022-06-18 05:36:26 -0400

    feat: stubbed out routes for user endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express('Router');
 
 // setup routes
-router.use('/users', require('./user.route'));
-router.use('/posts', require('./post.route'));
-router.use('/cart', require('./cart.route'));
-router.use('/transactions', require('./transaction.route'));
+router.use('/api/users', require('./user.route'));
+router.use('/api/posts', require('./post.route'));
+router.use('/api/cart', require('./cart.route'));
+router.use('/api/transactions', require('./transaction.route'));
 
 module.exports = router;
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,12 +5,30 @@ const router = express('Router');
 const logger = require('../middlewares/logger');
 router.use(logger.logger);
 
+/**
+ * Dealing with all users
+ */
 router.route('')
-    .get((req, res) => {
-        res.status(200).json({'greeting': 'Getting Users'});
-    })
-    .post((req, res) => {
-        res.status(200).json(req.body);
-    });
+    // get all users
+    .get((req, res) => {})
+    // create user
+    .post((req, res) => {});
 
+/**
+ * Dealing with one user
+ */
+router.route('/user/:userId')
+    // get user with id
+    .get((res, req) => {})
+    // update user with id
+    .put((res, req) => {})
+    // delete user with id
+    .delete((res, req) => {})
+
+/**
+ * Finding users with query
+ */
+router.get('/search', (res, req) => {})
+
+// export user router
 module.exports = router;
