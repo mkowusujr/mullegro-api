ffb3cd9effd1c63171270ed1c59daf076ab5687e
mathew owusu jr <mrfriiday@gmail.com>
2022-06-23 05:01:37 -0400

    feat: stubbed out post route and controller

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,50 @@
+const db = require('../models');
+const Post = require('../models/post')
+
+
+/**
+ * Get loggin in users posts
+ */
+exports.getCurrentUserPosts = async (currentUser) => {
+
+};
+
+
+/**
+ * Loggin in user create a new post
+ */
+exports.createNewPost = async (currentUser, newPost) => {
+
+};
+
+
+/**
+ * Loggin in user updating one of their posts
+ */
+exports.updatePost = async (currentUser, postId, updatedPost) => {
+
+};
+
+
+/**
+ * Loggin in user deleting one of their posts
+ */
+exports.deletePost = async (currentUser, postId) => {
+
+};
+
+
+/**
+ * Get all Posts
+ */
+exports.getPosts = async () => {
+
+};
+
+
+/**
+ * Get one post
+ */
+exports.getPost = async () => {
+
+};
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,16 +1,53 @@
 const express = require('express');
 const router = express('Router');
+const auth = require('../middlewares/auth');
+
+/**
+ * Logged in user getting and making new posts
+ */
+router.route('/users/user/posts')
+.get(auth.verifyToken, (req, res) => {
+
+})
+.post(auth.verifyToken, (req, res) => {
+
+});
+
+
+/**
+ * Logged in user modifying their posts
+ */
+router.route('/users/user/posts/:postId')
+.put(auth.verifyToken, (req, res) => {
+
+})
+.delete(auth.verifyToken, (req, res) => {
+
+});
+
 
 /**
  * Get all Posts
  */
-router.get('', (req, res) =>{
+router.get('/posts', (req, res) =>{
 
 });
 
+
 /**
- * Get all users post
+ * Get all a users post
  */
-router.get('/user/:username/posts')
+router.get('users/user/:username/posts', (req, res) => {
+
+})
+
+
+/**
+ * Get one post
+ */
+router.get('/posts/post/:id', (req, res) => {
+
+});
+
 
 module.exports = router;
