f03151a9275afe310e365a19d525a6cc1e2df0e5
mathew owusu jr <mrfriiday@gmail.com>
2022-07-25 19:41:27 -0400

    made all tests pending

--- test/spec/services/user.service.spec.js
+++ test/spec/services/user.service.spec.js
@@ -13,4 +13,76 @@ describe('User Service', () => {
         await db.sequelize.sync({ force: true });
         spyOn(console, 'error');
     });
+
+    describe('createUser', () => {
+        it('', async() => {
+            pending();
+        });        
+        it('', async() => {
+            pending();
+        });
+    });
+    
+    describe('getUser', () => {
+        it('', async() => {
+            pending();
+        });        
+        it('', async() => {
+            pending();
+        });
+    });
+
+    describe('getUserByEmail', () => {
+        it('', async() => {
+            pending();
+        });        
+        it('', async() => {
+            pending();
+        });
+    });
+    
+    describe('getUserByUsername', () => {
+        it('', async() => {
+            pending();
+        });        
+        it('', async() => {
+            pending();
+        });
+    });
+
+    describe('getUserAcctDetails', () => {
+        it('', async() => {
+            pending();
+        });        
+        it('', async() => {
+            pending();
+        });
+    });
+    
+    describe('getCurrentUser', () => {
+        it('', async() => {
+            pending();
+        });        
+        it('', async() => {
+            pending();
+        });
+    });
+
+    describe('findAll', () => {
+        it('', async() => {
+            pending();
+        });        
+        it('', async() => {
+            pending();
+        });
+    });
+    
+    describe('deleteUser', () => {
+        it('', async() => {
+            pending();
+        });        
+        it('', async() => {
+            pending();
+        });
+    });
 });
