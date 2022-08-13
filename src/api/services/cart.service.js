92ecbb29ab438e100ef0dee90befb44100e38ad9
mathew owusu jr <mrfriiday@gmail.com>
2022-08-12 23:37:41 -0400

    added and tested remove from cart

--- src/api/services/cart.service.js
+++ src/api/services/cart.service.js
@@ -4,27 +4,47 @@ const Post = db.posts;
 const helperService = require('./helper.service');
 
 exports.createCart = async (user) => {
-    try {
-        await user.createCart();
-        return Promise.resolve('Created cart successfully');
-    } catch (error) {
-        let errorOutput = 'Error creating cart: ' + error;
-        return helperService.sendRejectedPromiseWith(errorOutput);
-    }
-}
+  try {
+    await user.createCart();
+    return Promise.resolve('Created cart successfully');
+  } catch (error) {
+    let errorOutput = 'Error creating cart: ' + error;
+    return helperService.sendRejectedPromiseWith(errorOutput);
+  }
+};
 
 exports.addToCart = async (user, postId) => {
-    try {
-        let userCart = await user.getCart();
-        if (!userCart) throw `User with id of ${user.id} doesn\'t have a cart`;
-        
-        let post = await Post.findByPk(postId)
-        if(!post)  throw `Post with id of ${postId} does not exist`;
-        
-        await userCart.addPost(postId);
-        return Promise.resolve(`Successfully add post with id ${postId} to cart belonging to user with id of ${user.id}`)
-    } catch (error) {
-        let errorOutput = 'Error adding to cart: ' + error;
-        return helperService.sendRejectedPromiseWith(errorOutput);
-    }
-}
+  try {
+    let userCart = await user.getCart();
+    if (!userCart) throw `User with id of ${user.id} doesn\'t have a cart`;
+
+    let post = await Post.findByPk(postId);
+    if (!post) throw `Post with id of ${postId} does not exist`;
+
+    await userCart.addPost(postId);
+    return Promise.resolve(
+      `Successfully added post with id ${postId} to cart belonging to user with id of ${user.id}`
+    );
+  } catch (error) {
+    let errorOutput = 'Error adding to cart: ' + error;
+    return helperService.sendRejectedPromiseWith(errorOutput);
+  }
+};
+
+exports.removeFromCart = async (user, postId) => {
+  try {
+    let userCart = await user.getCart();
+    if (!userCart) throw `User with id of ${user.id} doesn\'t have a cart`;
+
+    let post = await Post.findByPk(postId);
+    if (!post) throw `Post with id of ${postId} does not exist`;
+
+    await userCart.removePost(postId);
+    return Promise.resolve(
+      `Successfully removed post with id ${postId} to cart belonging to user with id of ${user.id}`
+    );
+  } catch (error) {
+    let errorOutput = 'Error adding to cart: ' + error;
+    return helperService.sendRejectedPromiseWith(errorOutput);
+  }
+};
--- test/spec/services/cart.service.spec.js
+++ test/spec/services/cart.service.spec.js
@@ -3,115 +3,139 @@ const userService = require('../../../src/api/services/user.service');
 const postService = require('../../../src/api/services/post.service');
 
 describe('Cart Service', () => {
-    it('should be created', () => {
-      expect(cartService).toBeTruthy();
+  it('should be created', () => {
+    expect(cartService).toBeTruthy();
+  });
+
+  let db, Cart, User, Post, dummyUser, dummyUserCart;
+
+  beforeEach(async () => {
+    db = require('../../../src/api/models/index');
+    Cart = db.carts;
+    User = db.users;
+    Post = db.posts;
+    await db.sequelize.sync({ force: true });
+    spyOn(console, 'error');
+
+    dummyUser = await User.create({
+      name: 'Dummy User',
+      address: 'USA',
+      username: 'dummy_username',
+      email: 'dummay@email.com'
     });
+    await cartService.createCart(dummyUser);
+    dummyUserCart = await dummyUser.getCart();
+  });
 
-    let db, Cart, User, Post;
+  describe('createCart', () => {
+    it('creates a cart for a user', async () => {
+      let dummyUser2 = await User.create({
+        name: 'Dummy User',
+        address: 'USA',
+        username: 'dummy_username2',
+        email: 'dummay2@email.com'
+      });
 
-    beforeEach(async () => {
-        db = require('../../../src/api/models/index');
-        Cart = db.carts;
-        User = db.users;
-        Post = db.posts;
-        await db.sequelize.sync({ force: true });
-        spyOn(console, 'error');
+      let response = await cartService.createCart(dummyUser2);
+      let cart = await Cart.findByPk(2);
+
+      expect(cart.userId).toBe(dummyUser2.id);
+      expect(response).toBe('Created cart successfully');
     });
-
-    describe('createCart', () => {
-        it('creates a cart for a user', async () => {
-            let dummyUser = await User.create({
-                name: 'Dummy User',
-                address: 'USA',
-                username: 'dummy_username',
-                email: 'dummay@email.com'
-            });
-
-            let response = await cartService.createCart(dummyUser);
-            let cart = await Cart.findByPk(1);
-
-            expect(cart.userId).toBe(dummyUser.id)
-            expect(response).toBe('Created cart successfully');
-        });
-        it('throws an error if there is an issue', async () => {
-            try {
-                let dummyUser = {};
-                let response = await cartService.createCart(dummyUser);
-                if (response || !response) fail("Didn't throw error");
-              } catch (error) {
-                expect(console.error).toHaveBeenCalled();
-              }
-        });
+    it('throws an error if there is an issue', async () => {
+      try {
+        let dummyUser = {};
+        let response = await cartService.createCart(dummyUser);
+        if (response || !response) fail("Didn't throw error");
+      } catch (error) {
+        expect(console.error).toHaveBeenCalled();
+      }
     });
+  });
 
-    describe('addToCart', () => {
-        it('adds one item to the cart', async () => {
-            let dummyUser = await User.create({
-                name: 'Dummy User',
-                address: 'USA',
-                username: 'dummy_username',
-                email: 'dummay@email.com'
-            });
-            cartService.createCart(dummyUser);
-            let post = await Post.create({
-                title: 'Dummy Post',
-                price: 100.0,
-                description: 'This is an instrument',
-                condition: 'Good',
-                address: 'USA',
-                type: 'Clarinet',
-                status: 'Not Sold'
-              });
-              let postId = 1;
-
-              await cartService.addToCart(dummyUser, postId);
-              
-              expect(post.cartId).toBe(dummyUser.cartId)
-              expect(console.error).not.toHaveBeenCalled();
+  describe('addToCart', () => {
+    it('adds one item to the cart', async () => {
+      try {
+        let post = await Post.create({
+          title: 'Dummy Post',
+          price: 100.0,
+          description: 'This is an instrument',
+          condition: 'Good',
+          address: 'USA',
+          type: 'Clarinet',
+          status: 'Not Sold'
         });
-        it('throws an error if there is an issue', async () => {
-            try {
-                let dummyUser = await User.create({
-                    name: 'Dummy User',
-                    address: 'USA',
-                    username: 'dummy_username',
-                    email: 'dummay@email.com'
-                });
-                cartService.createCart(dummyUser);
-                let postId = 1;
+        let postId = 1;
 
-                await cartService.addToCart(dummyUser, postId);
-                if (response || !response) fail("Didn't throw error");
-              } catch (error) {
-                expect(console.error).toHaveBeenCalled();
-              }
-        });
+        await cartService.addToCart(dummyUser, postId);
+
+        expect(post.cartId).toBe(dummyUser.cartId);
+        expect(console.error).not.toHaveBeenCalled();
+      } catch (error) {
+        fail(error);
+      }
     });
+    it('throws an error if there is an issue', async () => {
+      try {
+        let postId = 1;
 
-    describe('removeFromCart', () => {
-        it('removes one item from the cart', async () => {
-            pending()
-        });
-        it('throws an error if there is an issue', async () => {
-            pending()
-        });
+        await cartService.addToCart(dummyUser, postId);
+        if (response || !response) fail("Didn't throw error");
+      } catch (error) {
+        expect(console.error).toHaveBeenCalled();
+      }
     });
+  });
 
-    describe('getCartItems', () => {
-        it('gets all the items in the cart', async () => {
-            pending()
+  describe('removeFromCart', () => {
+    it('removes one item from the cart', async () => {
+      try {
+        let post = await Post.create({
+          title: 'Dummy Post',
+          price: 100.0,
+          description: 'This is an instrument',
+          condition: 'Good',
+          address: 'USA',
+          type: 'Clarinet',
+          status: 'Not Sold'
         });
-        it('throws an error if there is an issue', async () => {
-            pending()
-        });
-    });
+        let postId = 1;
+        await cartService.addToCart(dummyUser, postId);
 
-    describe('clearCart', () => {
-        it('removes all the items in the cart', async () => {
-            pending()
-        });
-        it('throws an error if there is an issue', async () => {
-            pending()
-        });
+        await cartService.removeFromCart(dummyUser, postId);
+
+        expect(await dummyUserCart.getPosts()).toEqual([]);
+      } catch (error) {
+        fail(error);
+      }
     });
+    it('throws an error if there is an issue', async () => {
+      try {
+        let postId = 1;
+
+        await cartService.removeFromCart(dummyUser, postId);
+        if (response || !response) fail("Didn't throw error");
+      } catch (error) {
+        expect(console.error).toHaveBeenCalled();
+      }
+    });
+  });
+
+  describe('getCartItems', () => {
+    it('gets all the items in the cart', async () => {
+      pending();
+    });
+    it('throws an error if there is an issue', async () => {
+      pending();
+    });
+  });
+
+  describe('clearCart', () => {
+    it('removes all the items in the cart', async () => {
+      pending();
+    });
+    it('throws an error if there is an issue', async () => {
+      pending();
+    });
+  });
 });
