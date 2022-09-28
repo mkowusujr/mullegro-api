b009fe22f9982e1b311daf6c0da6264b64d22f4c
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-09-28 10:19:11 -0400

    Added doc comments

--- src/api/services/helper.service.js
+++ src/api/services/helper.service.js
@@ -1,3 +1,8 @@
+/**
+ * Helper function to log errors to return rejected promisese
+ * @param {string} errOutput The error message
+ * @returns A rejected Promise
+ */
 exports.sendRejectedPromiseWith = errOutput => {
   console.error(errOutput);
   return Promise.reject(new Error(errOutput));
