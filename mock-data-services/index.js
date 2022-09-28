008addffc94936b9e3033a012b41c30e3be0eb77
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-09-27 21:26:53 -0400

    Increased amount of mock users created

--- mock-data-services/index.js
+++ mock-data-services/index.js
@@ -2,6 +2,6 @@ const MockUserService = require('./mock-user.service');
 const MockPostService = require('./mock-post.service');
 
 exports.createMockData = async () => {
-  let users = await MockUserService.createMockUsers(10);
+  let users = await MockUserService.createMockUsers(25);
   await MockPostService.createPosts(users);
 };
